3f7fa1eba73164fa5cf8971f7a8c3a944f86811c
Victor Shevtsov <victorshevtsov@gmail.com>
2023-02-02 01:10:00 +0000

    Define aliases and models as const objects instead of JSONs

--- packages/datamodels/src/index.ts
+++ packages/datamodels/src/index.ts
@@ -1,3 +1,3 @@
-export { default as CampaignAliases } from "./aliases/Campaign.json" assert { type: "json" };
-export { default as WalletAliases } from "./aliases/Wallet.json" assert { type: "json" };
-export { default as WalletModel } from "./models/Wallet.json" assert { type: "json" };
+export { CampaignAliases } from "./aliases/Campaign.js";
+export { WalletAliases } from "./aliases/Wallet.js";
+export { WalletModel } from "./models/Wallet.js";
